3ac37f113fb2cadd74dfccd6b5515996de9c5c98
Michael Beaulieu <michael.beaulieu@hotmail.com>
2015-09-22 20:51:42 -0400

    Added project properties to git ignore

---
+++ IFT605TP2-Common/src/contracts/IDerivationCommands.java
@@ -0,0 +1,13 @@
+
+package contracts;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import udes.ds.agent.Equation;
+
+/**
+ *
+ */
+public interface IDerivationCommands extends Remote {
+    Equation Derivate(Equation e) throws RemoteException;
+}
